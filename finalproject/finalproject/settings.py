6e1188b00cb574bc4c4d45cf750d92758caa4b59
Rathi <brathi303@gmail.com>
2024-10-09 10:37:11 +0530

    projec4

--- finalproject/finalproject/settings.py
+++ finalproject/finalproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
@@ -144,3 +146,5 @@ EMAIL_HOST_PASSWORD='ipkx pecx tanw wind'
 
 RAZORPAY_KEY_ID = 'rzp_test_rW5AP7IlAu93C9'
 RAZORPAY_KEY_SECRET = 'k2cnoqOBwlshluV3vT1177FH'
+
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
